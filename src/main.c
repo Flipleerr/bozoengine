48e43d457406f4e836941107f33b0c34c861736f
memoryleak <glupacsuper2009@gmail.com>
2025-06-12 14:59:28 +0200

    improved window logic (should really squash these early commits

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <SDL3/SDL.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
@@ -8,12 +9,25 @@ int main(int argc, char *argv[]) {
     printf("failed to create window.");
     return 1;
   }
-  SDL_Event event;
-  while(SDL_PollEvent(&event)) {
-    // do nothing???
-  }
-  SDL_Delay(7000);
 
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+  bool running = true;
+  SDL_Event event;
+
+  while(running) {
+    while(SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
